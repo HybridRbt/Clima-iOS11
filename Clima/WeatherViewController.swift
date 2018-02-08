9daf1fb4a21ce110bfbf9355dd40aa5339e5e1ee
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2018-02-08 17:20:59 -0600

    replace the default appid with my own

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -13,7 +13,7 @@ class WeatherViewController: UIViewController {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "6df3099514dd9ffab1afef13228f1fa3"
     
 
     //TODO: Declare instance variables here
